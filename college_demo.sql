12fe2f8b617bccc97a07e66c265a8115c5acd873
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-08-17 22:29:30 +0530

    Update college_demo.sql

--- college_demo.sql
+++ college_demo.sql
@@ -37,3 +37,6 @@ alter table department add location varchar(100);
 alter table student add dob varchar(60);
 alter table student add gender varchar(30);
 alter table student add cgpa float;
+alter table course add credits int;
+alter table course add faculty_id int foreign key (faculty_id) references faculty(faculty_id);
+alter table course course_code varchar(60);
